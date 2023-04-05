22db28462df2f52106f443be1b2e2928225ce250
GaelSM <gaelsegura.mendoza@gmail.com>
2023-04-04 19:47:39 -0600

    base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Advice-Generator-App-/"
+  base: 'https://gaelsm.github.io/<REPO>/Advice-Generator-App/'
 })
